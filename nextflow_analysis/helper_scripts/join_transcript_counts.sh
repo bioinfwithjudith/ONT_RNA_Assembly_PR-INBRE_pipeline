6ddf9fe32937679703775cf986e589e259797795
Judith Rodriguez <59932471+bioinfwithjudith@users.noreply.github.com>
2026-06-27 18:53:08 -0700

    Update join_transcript_counts.sh

--- nextflow_analysis/helper_scripts/join_transcript_counts.sh
+++ nextflow_analysis/helper_scripts/join_transcript_counts.sh
@@ -1,11 +1,14 @@
 paste \
-<(cut -f1 SRR30955394_oarfish_quant.quant) \
-<(cut -f3 SRR30955394_oarfish_quant.quant) \
-<(cut -f3 SRR30955395_oarfish_quant.quant) \
-<(cut -f3 SRR30955396_oarfish_quant.quant) \
-<(cut -f3 SRR30955397_oarfish_quant.quant) \
-<(cut -f3 SRR30955398_oarfish_quant.quant) \
-<(cut -f3 SRR30955399_oarfish_quant.quant) \
-> transcript_counts.tsv
+<(cut -f1 /Volumes/X9_Pro/lvazquez_contract/differential_expression_local_analysis/results_replicates_lowerbands_barcode4_H1299/quantification/SRR30955394_oarfish_quant.quant) \
+<(cut -f3 /Volumes/X9_Pro/lvazquez_contract/differential_expression_local_analysis/results_replicates_lowerbands_barcode4_H1299/quantification/SRR30955394_oarfish_quant.quant) \
+<(cut -f3 /Volumes/X9_Pro/lvazquez_contract/differential_expression_local_analysis/results_replicates_lowerbands_barcode4_H1299/quantification/SRR30955395_oarfish_quant.quant) \
+<(cut -f3 /Volumes/X9_Pro/lvazquez_contract/differential_expression_local_analysis/results_replicates_lowerbands_barcode4_H1299/quantification/SRR30955396_oarfish_quant.quant) \
+<(cut -f3 /Volumes/X9_Pro/lvazquez_contract/differential_expression_local_analysis/results_replicates_lowerbands_barcode5_H1299/quantification/SRR30955375_oarfish_quant.quant) \
+<(cut -f3 /Volumes/X9_Pro/lvazquez_contract/differential_expression_local_analysis/results_replicates_lowerbands_barcode6_H1299/quantification/SRR30954502_oarfish_quant.quant) \
+<(cut -f3 /Volumes/X9_Pro/lvazquez_contract/differential_expression_local_analysis/results_replicates_lowerbands_barcode6_H1299/quantification/SRR30954503_oarfish_quant.quant) \
+<(cut -f3 /Volumes/X9_Pro/lvazquez_contract/differential_expression_local_analysis/results_replicates_lowerbands_barcode6_H1299/quantification/SRR30954504_oarfish_quant.quant) \
+<(cut -f3 /Volumes/X9_Pro/lvazquez_contract/differential_expression_local_analysis/results_replicates_lowerbands_barcode6_H1299/quantification/SRR30954505_oarfish_quant.quant) \
+> /Volumes/X9_Pro/lvazquez_contract/differential_expression_local_analysis/transcript_counts.tsv
 
-sed -i '' '1s/.*/transcript\tSRR30955394\tSRR30955395\tSRR30955396\tSRR30955397\tSRR30955398\tSRR30955399/' transcript_counts.tsv
+sed -i '' '1s/.*/transcript\tSRR30955394\tSRR30955395\tSRR30955396\tSRR30955375\tSRR30954502\tSRR30954503\tSRR30954504\tSRR30954505/' transcript_counts.tsv
+
